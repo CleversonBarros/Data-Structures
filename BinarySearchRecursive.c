408ba3bff0b13dfa93c84498c669da16ce8814c6
CleversonBarros <cfbn@ic.ufal.br>
2017-07-23 23:24:33 -0300

    Algorithm Binary Search Recursive

---
+++ BinarySearchRecursive.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int binarySearch(int *v, int left, int right, int key)
+{
+	int mid = (left+right)/2;
+	if(left > right) return -1;
+	else
+	{
+		if(v[mid] == key)
+		{
+			return mid;
+		}
+		else if(v[mid] < key)
+		{
+			binarySearch(v,mid+1,right,key);
+		}
+		else
+		{
+			binarySearch(v,left,mid-1,key);
+		}
+	}
+	
+}
+
+int main()
+{
+	int v[]={1,2,3,4,5}, a;
+	
+	a = binarySearch(v,0,5,5);
+	if(a == -1)
+	{
+		printf("Não encontrado!\n");
+	}
+	else
+	{
+		printf("Encontrado, posicao: %d\n",a);
+	}
+	
+}
